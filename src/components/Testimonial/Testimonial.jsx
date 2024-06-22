61653dbd515907e2adfba0261ae1e515c78aac38
ProdipKumarShil <prodipkrishna01@gmail.com>
2024-06-22 21:02:30 +0600

    added testimonial animation

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,18 +1,50 @@
 import start from '../../assets/icons/star.svg'
-
+import { motion } from 'framer-motion'
 
 const Testimonial = () => {
+
+  const parentVarient = {
+    initial: {},
+    animate: {
+      transition: {
+        staggerChildren: 0.5
+      }
+    }
+  }
+
+  const childVarient = {
+    initial: {
+      opacity: 0,
+      x: 100,
+      y: 100,
+      scale: 0.8,
+      rotate: 45
+    },
+    animate: {
+      opacity: 1,
+      x: 0,
+      y: 0,
+      scale: 1,
+      rotate: 0
+    }
+  }
+
   return (
     <div className="screen-lg py-[120px]">
       <p className="font-bold text-[48px] text-black mb-6">Customer testimonials</p>
       <p className="text-[#494949] mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
       {/* Review cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <ReviewCard />
-        <ReviewCard />
-        <ReviewCard />
-      </div>
+      <motion.div
+        variants={parentVarient}
+        viewport={{once: true, amount: 0.4}}
+        initial='initial'
+        whileInView='animate'
+        className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <ReviewCard childVarient={childVarient} />
+        <ReviewCard childVarient={childVarient} />
+        <ReviewCard childVarient={childVarient} />
+      </motion.div>
 
     </div>
   )
@@ -20,9 +52,9 @@ const Testimonial = () => {
 
 export default Testimonial
 
-const ReviewCard = () => {
+const ReviewCard = ({ childVarient }) => {
   return (
-    <div className="p-8 shadow-md rounded">
+    <motion.div variants={childVarient} className="p-8 shadow-md rounded">
       <img className='mb-6' src={start} alt="" />
       <p className='text-[14px] text-[#494949] mb-6'>&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.&quot;</p>
       <div className="flex gap-5 items-center">
@@ -32,6 +64,6 @@ const ReviewCard = () => {
           <p className='text-[#494949]'>Marketing</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
